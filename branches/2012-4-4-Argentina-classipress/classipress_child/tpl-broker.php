4b80fc9a50fc57bef7a9484930917b9cde04fc6a
nacheal <nacheal@gmail.com>
2012-04-19 04:00:10 +0000

    import classipress_child theme

---
+++ branches/2012-4-4-Argentina-classipress/classipress_child/tpl-broker.php
@@ -0,0 +1,276 @@
+<?php
+/*
+Template Name: Broker Template
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="content">
+
+    <div class="content_botbg">
+
+        <div class="content_res">
+
+			<div id="breadcrumb">
+
+                  <?php if ( function_exists('cp_breadcrumb') ) cp_breadcrumb(); ?>
+
+              </div>
+
+
+            <!-- left block -->
+            <div class="content_left">
+
+                <div class="shadowblock_out">
+
+                    <div class="shadowblock">
+
+                        <h1 class="single dotted"><?php _e('Brokers','appthemes')?> </h1>
+                        
+                        
+                        <h3> Brokers y empresas que confían en nuestros servicios:</h3>
+                        <br/>
+                        
+                        <p>Si su negocio no figura en nuestro directorio, puede solicitar un espacio <a>aquí</a></p>
+                        
+                        <?php if(function_exists('wp_custom_fields_search')) 
+	wp_custom_fields_search(); ?>
+
+                        <div class="post">
+
+							<div id="user-photo"><img class="avatar avatar-96 current-author photo" width="96" height="96" src="http://mercadobarcos.com/wp-content/uploads/2012/02/kbyachts_avatar_1-96x96.jpg" alt="kbyachts"></div>
+
+                            <div class="author-main">
+
+								<ul class="author-info">
+									<li><h2>KB Yachts</h2></li>
+									
+                                    <li><strong><?php _e('Broker','appthemes');?></strong></li>
+                                  <li><strong><?php _e('Ciudad:','appthemes');?></strong> Nordelta</li>
+                                    <li><strong><?php _e('Teléfono:','appthemes');?></strong> 15 4986 8669</li>
+                                    <li><strong><a href="http://mercadobarcos.com/author/kbyachts/"><?php _e('Ir al perfil','appthemes');?></a></strong> </li>
+							  </ul>
+
+                            </div>
+                            
+                             </div><!--/directory-->
+                             
+                             
+                             
+                            <div class="post">
+
+							<div id="user-photo"><img class="avatar avatar-96 current-author photo" width="96" height="96" src="http://mercadobarcos.com/wp-content/uploads/2012/02/donatobarcos_avatar-96x96.jpg" alt="donatobarcos"></div>
+
+                            <div class="author-main">
+
+								<ul class="author-info">
+									<li><h2>Donato Barcos</h2></li>
+									
+                                    <li><strong><?php _e('Negocio','appthemes');?></strong></li>
+                                  <li><strong><?php _e('Ciudad:','appthemes');?></strong> Rosario</li>
+                                    <li><strong><?php _e('Teléfono:','appthemes');?></strong> 0341155113509</li>
+                                    <li><strong><a href="http://mercadobarcos.com/author/donatobarcos/"><?php _e('Ir al perfil','appthemes');?></a></strong> </li>
+							  </ul>
+
+                            </div>
+                            
+                             </div><!--/directory-->
+                             
+                             
+                             
+
+                          
+                            <div class="post">
+
+							<div id="user-photo"><img class="avatar avatar-96 photo" width="96" height="96" src="http://1.gravatar.com/avatar/5b411af287107e41f841750d5977fa01?s=96&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G" alt=""></div>
+
+                            <div class="author-main">
+
+								<ul class="author-info">
+									<li><h2>RGnaval</h2></li>
+									
+                                    <li><strong><?php _e('Astillero','appthemes');?></strong></li>
+                                  <li><strong><?php _e('Ciudad:','appthemes');?></strong> Goya</li>
+                                    <li><strong><?php _e('Teléfono:','appthemes');?></strong> 03777-15600000</li>
+                                    <li><strong><a href="http://mercadobarcos.com/author/rgnaval/"><?php _e('Ir al perfil','appthemes');?></a></strong> </li>
+							  </ul>
+
+                            </div>
+                            
+                             </div><!--/directory-->
+                             
+                             
+                             
+                             
+                             <div class="post">
+
+							<div id="user-photo"><img class="avatar avatar-96 current-author photo" width="96" height="96" src="http://mercadobarcos.com/wp-content/uploads/2012/02/nuevooriente_avatar-96x96.jpg" alt="nuevooriente"></div>
+
+                            <div class="author-main">
+
+								<ul class="author-info">
+									<li><h2>Náutica Nuevo Oriente</h2></li>
+									
+                                    <li><strong><?php _e('Broker','appthemes');?></strong></li>
+                                  <li><strong><?php _e('Ciudad:','appthemes');?></strong> Nordelta</li>
+                                    <li><strong><?php _e('Teléfono:','appthemes');?></strong> 15-6095-6375</li>
+                                    <li><strong><a href="http://mercadobarcos.com/author/nuevooriente/"><?php _e('Ir al perfil','appthemes');?></a></strong> </li>
+							  </ul>
+
+                            </div>
+                            
+                             </div><!--/directory-->
+                             
+                             
+                             <div class="post">
+
+							<div id="user-photo"><img class="avatar avatar-96 photo" width="96" height="96" src="http://1.gravatar.com/avatar/5b411af287107e41f841750d5977fa01?s=96&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G" alt="Jorplast"></div>
+
+                            <div class="author-main">
+
+								<ul class="author-info">
+									<li><h2>Jorplast</h2></li>
+									
+                                    <li><strong><?php _e('Negocio','appthemes');?></strong></li>
+                                  <li><strong><?php _e('Ciudad:','appthemes');?></strong> Vicente Lopez</li>
+                                    <li><strong><?php _e('Teléfono:','appthemes');?></strong> 1134118917- 1540968917</li>
+                                    <li><strong><a href="http://mercadobarcos.com/author/jorplast/"><?php _e('Ir al perfil','appthemes');?></a></strong> </li>
+							  </ul>
+
+                            </div>
+                            
+                             </div><!--/directory-->
+                             
+                             
+                             
+                             <div class="post">
+
+							<div id="user-photo"><img class="avatar avatar-96 current-author photo" width="96" height="96" src="http://mercadobarcos.com/wp-content/uploads/2012/02/FABIOPACHE_avatar-96x96.png" alt="FABIOPACHE"></div>
+
+                            <div class="author-main">
+
+								<ul class="author-info">
+									<li><h2>Astillero Diluvio</h2></li>
+									
+                                    <li><strong><?php _e('Broker','appthemes');?></strong></li>
+                                  <li><strong><?php _e('Ciudad:','appthemes');?></strong> Rosario</li>
+                                    <li><strong><?php _e('Teléfono:','appthemes');?></strong> 0341-153223444</li>
+                                    <li><strong><a href="http://mercadobarcos.com/author/fabiopache/"><?php _e('Ir al perfil','appthemes');?></a></strong> </li>
+							  </ul>
+
+                            </div>
+                            
+                             </div><!--/directory-->
+                             
+                             
+                             
+                             <div class="post">
+
+							<div id="user-photo"><img class="avatar avatar-96 current-author photo" width="96" height="96" src="http://mercadobarcos.com/wp-content/uploads/2012/02/DELTAYACHTS_avatar-96x96.jpg" alt="DELTAYACHTS"></div>
+
+                            <div class="author-main">
+
+								<ul class="author-info">
+									<li><h2>Deltayachts</h2></li>
+									
+                                    <li><strong><?php _e('Broker','appthemes');?></strong></li>
+                                  <li><strong><?php _e('Ciudad:','appthemes');?></strong> Lanús Oeste</li>
+                                    <li><strong><?php _e('Teléfono:','appthemes');?></strong> 15 5303-8505</li>
+                                    <li><strong><a href="http://mercadobarcos.com/author/deltayachts/"><?php _e('Ir al perfil','appthemes');?></a></strong> </li>
+							  </ul>
+
+                            </div>
+                            
+                             </div><!--/directory-->
+                             
+                             
+                             
+                             <div class="post">
+
+							<div id="user-photo"><img class="avatar avatar-96 current-author photo" width="96" height="96" src="http://mercadobarcos.com/wp-content/uploads/2012/02/jorreg_avatar-96x96.jpg" alt="jorreg"></div>
+
+                            <div class="author-main">
+
+								<ul class="author-info">
+									<li><h2>Jorge Regnicoli</h2></li>
+									
+                                    <li><strong><?php _e('Individual','appthemes');?></strong></li>
+                                  <li><strong><?php _e('Ciudad:','appthemes');?></strong> San Fernando</li>
+                                    <li><strong><?php _e('Teléfono:','appthemes');?></strong> 1563818723</li>
+                                    <li><strong><a href="http://mercadobarcos.com/author/jorreg/"><?php _e('Ir al perfil','appthemes');?></a></strong> </li>
+							  </ul>
+
+                            </div>
+                            
+                             </div><!--/directory-->
+                             
+                             
+                             
+                             <div class="post">
+
+							<div id="user-photo"><img class="avatar avatar-96 current-author photo" width="96" height="96" src="http://mercadobarcos.com/wp-content/uploads/2012/02/Marina_avatar-96x96.jpg" alt="Marina"></div>
+
+                            <div class="author-main">
+
+								<ul class="author-info">
+									<li><h2>Marina Coronel</h2></li>
+									
+                                    <li><strong><?php _e('Negocio','appthemes');?></strong></li>
+                                  <li><strong><?php _e('Ciudad:','appthemes');?></strong> Rosario</li>
+                                    <li><strong><?php _e('Teléfono:','appthemes');?></strong> 0341-156183502</li>
+                                    <li><strong><a href="http://mercadobarcos.com/author/marina/"><?php _e('Ir al perfil','appthemes');?></a></strong> </li>
+							  </ul>
+
+                            </div>
+                            
+                             </div><!--/directory-->
+                             
+                             
+                             
+                             
+                             <div class="post">
+
+						<div id="user-photo"><img class="avatar avatar-96 photo" width="96" height="96" src="http://1.gravatar.com/avatar/5b411af287107e41f841750d5977fa01?s=96&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G" alt="Elmarlin"></div>
+
+                            <div class="author-main">
+
+								<ul class="author-info">
+									<li><h2>Náutica El Marlin</h2></li>
+									
+                                    <li><strong><?php _e('Broker','appthemes');?></strong></li>
+                                  <li><strong><?php _e('Ciudad:','appthemes');?></strong> Mar de Ajó</li>
+                                    <li><strong><?php _e('Teléfono:','appthemes');?></strong> (02257) 42 211</li>
+                                    <li><strong><a href="http://mercadobarcos.com/author/elmarlin/"><?php _e('Ir al perfil','appthemes');?></a></strong> </li>
+							  </ul>
+
+                            </div>
+                            
+                             </div><!--/directory-->
+                             
+
+							
+
+                       
+
+                    </div><!-- /shadowblock -->
+
+                </div><!-- /shadowblock_out -->
+
+            </div><!-- /content_left -->
+
+
+            <?php get_sidebar(); ?>
+
+            <div class="clr"></div>
+
+
+        </div><!-- /content_res -->
+
+    </div><!-- /content_botbg -->
+
+</div><!-- /content -->
+
+
+<?php get_footer(); ?>
+

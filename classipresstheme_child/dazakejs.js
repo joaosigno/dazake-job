5808e16de2aec7b19d59187b5376deacc4bba930
bryantandk <bryantandk@gmail.com>
2012-04-20 17:49:36 +0000

    make second slider slide

--- classipresstheme_child/dazakejs.js
+++ classipresstheme_child/dazakejs.js
@@ -5,6 +5,39 @@ jQuery(document).ready(function(jQuery){
 	var pause = 3500;
 	var interval="";
 
+	var dazakeSlider = {
+		init: function(){
+			this.el = $("#dazake-mid-slider");
+			this.el.length = this.el.children('a').length;
+			this.width = (this.el.length+1) * 220;
+			this.space = 225;
+			this.interval = 2500;
+			this.t = "";
+			this.dir = true;
+			this.el.css('width',this.width);
+			this.start();
+		},
+		start: function(){
+			var self = this;
+
+			this.t = setInterval(function(){
+				var thisMargin = parseInt(self.el.css('margin-left'));
+				// console.log(self.el.length);
+				
+				self.el.animate({'margin-left': thisMargin-self.space});
+
+				if( Math.abs(thisMargin) ===  (self.el.length - 5) * self.space){ self.reverse(); }
+			}, this.interval);
+		},
+		stop: function(){
+			clearInterval(this.t);
+		},
+		reverse: function(){
+			// this.dir = !this.dir;
+			this.el.animate({'margin-left': 0});
+		}
+	}
+
 	function removeFirst()
 	{		
 			first = $('.dazakeactive div:first').html();
@@ -42,7 +75,7 @@ jQuery(document).ready(function(jQuery){
 			else dazakeStop();
 	}
 		
-	$('ul#listticker1').hover(function(){
+	$('.dazakeactive').hover(function(){
 			dazakeStop();
 		}, function(){
 			dazakeStart();
@@ -50,11 +83,18 @@ jQuery(document).ready(function(jQuery){
 
 	
 	dazakeStart();
+	dazakeSlider.init();
+	$('#dazake-mid-slider').mouseover(function(){
+		dazakeSlider.stop();
+	});
+	$('#dazake-mid-slider').mouseout(function(){
+		dazakeSlider.start();
+	});
+	// console.log($("#dazake-mid-slider img").length);
 	
 	$('.tabnavig li a').click(function(){
 		var tabName = $(this).attr('href').slice(1,7);
 		$('.dazake_slideup_block').removeClass('dazakeactive');
 		$('#'+tabName).children('.dazake_slideup_block').addClass('dazakeactive');
 	})
-	//interval = setInterval(removeFirst, pause);
 });

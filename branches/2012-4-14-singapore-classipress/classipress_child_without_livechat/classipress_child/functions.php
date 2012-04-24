00a2b2fb9824c7d64886e1602ac4e222dec12675
nacheal@gmail.com <nacheal@gmail.com@6be763a1-2304-9f22-45ae-06bf7fe9b562>
2012-04-24 10:41:36 +0000

    add child theme without live chat

---
+++ branches/2012-4-14-singapore-classipress/classipress_child_without_livechat/classipress_child/functions.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ * Write your own functions or modify functions, actions, and filters using this file.
+ * LIST YOUR FUNCTIONS (optional):
+ */
+
+//Place All Your Custom Function Below This Line
+
+//change price to spit out correct looking currency and ignore anything that's not a price.
+
+
+//add js script 
+function my_scripts_method() {
+      $dazakejsurl =  get_bloginfo('stylesheet_url');
+      $dazakejsurl = str_replace('style.css', 'dazakejs.js',$dazakejsurl);
+  wp_enqueue_script(
+    'dazakejs',
+    $dazakejsurl,
+    array('scriptaculous')
+  );
+}    
+
+
+add_action('wp_enqueue_scripts', 'my_scripts_method');
+
+
+//add custom meta box 
+add_action( 'add_meta_boxes', 'myplugin_add_custom_box' );
+/* Do something with the data entered */
+add_action( 'save_post', 'myplugin_save_postdata' );
+
+/* Adds a box to the main column on the Post and Page edit screens */
+function myplugin_add_custom_box() {
+    add_meta_box( 
+        'myplugin_sectionid',
+        __( 'Livechat* :', 'appthemes' ),
+        'myplugin_inner_custom_box',
+        'ad_listing' 
+    );
+}
+
+/* Prints the box content */
+function myplugin_inner_custom_box( $post ) {
+  // Use nonce for verification
+  wp_nonce_field( plugin_basename( __FILE__ ), 'myplugin_noncename' );
+  $meta_values = get_post_meta($post->ID, 'myplugin_new_field', true); 
+  // The actual fields for data entry
+  echo '<label for="myplugin_new_field">';
+       _e("Livechat* :", 'appthemes' );
+  echo '</label> ';
+  echo '<textarea rows="10" cols="30" id="myplugin_new_field" name="myplugin_new_field">';
+  echo $meta_values;
+  echo '</textarea>';
+}
+
+/* When the post is saved, saves our custom data */
+function myplugin_save_postdata( $post_id ) {
+  if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
+      return;
+
+  if ( !wp_verify_nonce( $_POST['myplugin_noncename'], plugin_basename( __FILE__ ) ) )
+      return;
+
+  // Check permissions
+  if ( 'page' == $_POST['post_type'] ) 
+  {
+    if ( !current_user_can( 'edit_page', $post_id ) )
+        return;
+  }
+  else
+  {
+    if ( !current_user_can( 'edit_post', $post_id ) )
+        return;
+  }
+
+  // OK, we're authenticated: we need to find and save the data
+  $mydata = $_POST['myplugin_new_field'];
+  $mydata = htmlentities($mydata);
+
+  update_post_meta($post_id, 'myplugin_new_field', $mydata);
+}
+
+// initialize all the sidebars so they are widgetized
+function cp_dazake_sidebars_init() {
+    if ( !function_exists('register_sidebars') )
+        return;
+      
+      register_sidebar(array(
+        'name'          => __('Head Dazake Sidebar','appthemes'),
+        'id'            => 'sidebar_dazake_head',
+        'description'   => __('This is the head ClassiPress sidebar add by child theme.','appthemes'),
+        'before_widget' => '<div class="shadowblock_out" id="%2$s"><div id="dazake_header_wi">',
+        'after_widget'  => '</div><!-- /shadowblock --></div><!-- /shadowblock_out -->',
+        'before_title'  => '<h2 class="dotted">',
+        'after_title'   => '</h2>',
+    ));
+
+    register_sidebar(array(
+        'name'          => __('Middle Dazake Sidebar','appthemes'),
+        'id'            => 'sidebar_dazake_middle',
+        'description'   => __('This is the middle ClassiPress sidebar add by child theme.','appthemes'),
+        'before_widget' => '<div class="dazakeblockout shadowblock_out" id="%2$s"><div class="shadowblockdir"><div id="dazake_middle_wi">',
+        'after_widget'  => '</div><!-- /shadowblock --></div></div><!-- /shadowblock_out -->',
+        'before_title'  => '<h2 class="dotted">',
+        'after_title'   => '</h2>',
+    ));
+
+}
+
+add_action( 'wp_register_sidebar_widget', 'cp_dazake_sidebars_init' );
+
+//add submenu for adding local live chat
+function cp_live_chat(){
+  echo 'livechat';
+}
+// add_submenu_page( 'users.php', __('Live Chat','appthemes'), __('Live Chat','appthemes'), 'manage_options', 'live_chat', 'cp_live_chat' );
+
+add_action('admin_menu', 'register_my_custom_submenu_page');
+
+function register_my_custom_submenu_page() {
+  add_submenu_page( 'users.php', 'Live Chat', 'Live Chat', 'manage_options', 'live_chat', 'live_chat_callback' ); 
+}
+
+function live_chat_callback() {
+  if(isset($_POST['submit'])){
+      $mydata = $_POST['locallivechat'];
+      update_option( 'locallivechat', $mydata );
+  }
+  $locallivechat = get_option('locallivechat');
+  $locallivechat = stripslashes($locallivechat);
+  ?>
+  <div class="icon32" id="icon-tools"><br></div>
+  <h2>Local Live Chat Setting</h2>
+  <form action="?page=live_chat" method = "POST">
+    <p class="submit btop" >
+    <input type="submit" name = 'submit' value = "Save Changes">
+  </p>
+    <textarea style = "margin-top:20px" rows="10" cols="30" id="locallivechat" name="locallivechat"><?php echo $locallivechat ; ?></textarea>
+  </form>
+  <?php
+}
---
+++ branches/2012-4-14-singapore-classipress/classipress_child_without_livechat/classipress_child/index.php
@@ -0,0 +1,176 @@
+<?php get_header(); ?>
+
+<div class="content">
+
+	<div class="content_botbg">
+
+		<div class="content_res">
+
+		
+             <?php 
+				// if ( file_exists(STYLESHEETPATH . '/featured.php') )//
+	   //              include_once(STYLESHEETPATH . '/featured.php');
+   	//         else
+    //    	        include_once(TEMPLATEPATH . '/featured.php');
+			?>
+<?php  // if(function_exists("xtremecarousel")) {   xtremecarousel(); }?>
+
+<?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('sidebar_dazake_middle') ) : else : ?> <!-- no dynamic sidebar so don't do anything --> <?php endif; ?>
+
+
+
+        <!-- left block -->
+        <div class="content_left">
+
+
+            <div class="tabcontrol">
+
+                                    <ul class="tabnavig"> 
+              <li><a href="#block3"><span class="big"><?php _e('Featured Listings','appthemes')?></span></a></li> 
+              <li><a href="#block1"><span class="big"><?php _e('Just Listed','appthemes')?></span></a></li> 
+              <li><a href="#block2"><span class="big"><?php _e('Most Popular','appthemes')?></span></a></li> 
+              <li><a href="#block4"><span class="big"><?php _e('Categories','appthemes')?></span></a></li> 
+            </ul> 
+             
+        <?php remove_action( 'appthemes_after_endwhile', 'cp_do_pagination' ); ?> 
+      <?php $post_type_url = get_bloginfo('url').'/'.get_option('cp_post_type_permalink').'/'; ?> 
+
+            <!-- tab 1 --> 
+            <div id="block1"> 
+
+              <div class="clr"></div> 
+
+              <div class="undertab"><span class="big"><?php _e('Classified Ads','appthemes') ?> / <strong><span class="colour"><?php _e('Just Listed','appthemes') ?></span></strong></span></div> 
+
+              <div class="dazake_slideup_block">
+                <?php 
+                    // show all ads but make sure the sticky featured ads don't show up first 
+                    $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1; 
+                    query_posts( array('post_type' => APP_POST_TYPE, 'ignore_sticky_posts' => 1, 'paged' => $paged) ); 
+            global $wp_query; 
+            $total_pages = max( 1, absint( $wp_query->max_num_pages ) ); 
+                ?> 
+
+                <?php get_template_part( 'loop', 'ad_listing' ); ?> 
+              </div>
+        <?php 
+            if( $total_pages > 1 ){ ?> 
+            <div class="paging"><a href="<?php echo $post_type_url; ?>page/2/"> <?php _e( 'View More Ads', 'appthemes' ); ?> </a></div> 
+            <?php } ?> 
+              
+            </div><!-- /block1 --> 
+
+
+
+            <!-- tab 2 --> 
+            <div id="block2"> 
+
+              <div class="clr"></div> 
+
+              <div class="undertab"><span class="big"><?php _e('Classified Ads','appthemes') ?> / <strong><span class="colour"><?php _e('Most Popular','appthemes') ?></span></strong></span></div> 
+              <div class="dazake_slideup_block">
+        <?php get_template_part( 'loop', 'featured' ); ?> 
+              </div>
+        <?php global $cp_has_next_page; ?> 
+        <?php if($cp_has_next_page){ ?> 
+            <div class="paging"><a href="<?php echo $post_type_url; ?>page/2/?sort=popular"> <?php _e( 'View More Ads', 'appthemes' ); ?> </a></div> 
+        <?php } ?> 
+
+        <?php wp_reset_query(); ?> 
+              
+            </div><!-- /block2 --> 
+
+
+
+            <!-- tab 3 --> 
+            <div id="block3"> 
+
+              <div class="clr"></div> 
+
+              <div class="undertab"><span class="big"><?php _e('Classified Ads','appthemes') ?> / <strong><span class="colour"><?php _e('Featured Listings','appthemes') ?></span></strong></span></div> 
+
+
+              <div class="dazake_slideup_block dazakeactive">
+                <?php 
+                    // show all random ads but make sure the sticky featured ads don't show up first 
+                    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; 
+  
+                   query_posts(array('post_type' => 'ad_listing', 'ad_cat' => $term->slug, 'caller_get_posts' => 1, 'post__in' => get_option('sticky_posts'), 'paged' => $paged,)); 
+  
+                ?> 
+                <?php get_template_part( 'loop', 'ad_listing' ); ?> 
+                </div>
+        <?php 
+            if( $total_pages > 1 ){ ?> 
+            <div class="paging"><a href="<?php echo $post_type_url; ?>page/2/?sort=Featured Listings"> <?php _e( 'View More Ads', 'appthemes' ); ?> </a></div> 
+            <?php } ?> 
+                
+            </div><!-- /block3 --> 
+
+            <!-- tab 4 --> 
+            <div id="block4"> 
+              <div class="clr"></div> 
+  
+              <div class="undertab"><span class="big"><?php _e('Classified Ads','appthemes') ?> / <strong><span class="colour"><?php _e('Categories','appthemes')?></span></strong></span></div> 
+  
+              <div class="shadowblock_out"> 
+  
+                    <div class="shadowblock"> 
+  
+                        <h2 class="dotted"><?php _e('Ad Categories','appthemes')?></h2> 
+  
+                        <div id="directory" class="directory <?php if(get_option('cp_cat_dir_cols') == 2) echo 'two'; ?>Col"> 
+  
+                            <?php echo cp_cat_menu_drop_down(get_option('cp_cat_dir_cols'), get_option('cp_dir_sub_num')); ?> 
+  
+                            <div class="clr"></div> 
+  
+                        </div><!--/directory--> 
+  
+                    </div><!-- /shadowblock --> 
+  
+                </div><!-- /shadowblock_out --> 
+
+            </div><!-- /block4 -->
+
+
+
+              <div class="clr"></div>
+
+              <div class="undertab"><span class="big"><?php _e('Classified Ads','appthemes') ?> / <strong><span class="colour"><?php _e('Random','appthemes') ?></span></strong></span></div>
+
+                <?php
+                    // show all random ads but make sure the sticky featured ads don't show up first
+                    $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+                    query_posts( array('post_type' => APP_POST_TYPE, 'ignore_sticky_posts' => 1, 'paged' => $paged, 'orderby' => 'rand') );
+		    global $wp_query;
+		    $total_pages = max( 1, absint( $wp_query->max_num_pages ) );
+                ?>
+
+                <?php get_template_part( 'loop', 'ad_listing' ); ?>
+
+		<?php
+		    if( $total_pages > 1 ){ ?>
+			<div class="paging"><a href="ads/page/2/?sort=random"> <?php _e( 'View More Ads', 'appthemes' ); ?> </a></div>
+		    <?php } ?>
+
+            </div><!-- /block4 -->
+            
+          </div><!-- /tabcontrol -->
+
+      </div><!-- /content_left -->
+
+
+            <?php get_sidebar(); ?>
+
+
+            <div class="clr"></div>
+
+        </div><!-- /content_res -->
+
+    </div><!-- /content_botbg -->
+
+</div><!-- /content -->
+
+
+<?php get_footer(); ?>

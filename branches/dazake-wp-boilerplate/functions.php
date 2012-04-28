eacb53f83aae943ee5c7c7aeb1c2526de7cce90e
bryantandk <bryantandk@gmail.com>
2012-04-28 12:15:35 +0000

    add dazake-wp-boilerpalte

---
+++ branches/dazake-wp-boilerplate/functions.php
@@ -0,0 +1,41 @@
+<?php
+
+	/**
+	 * Ladda script
+	 */
+	function dazake_load_script() {
+		if (!is_admin()) {
+
+			/**
+			 * Deregister wordpress scripts
+			 */
+			wp_deregister_script('jquery');
+
+			/**
+			 * wp_register_script( $handle, $src, $deps, $ver, $in_footer );
+			 */
+			wp_register_script('jquery', 'http://lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js');
+			wp_register_script('modernizr', get_template_directory_uri() . '/js/libs/modernizr.js');
+			wp_register_script('bootstrap', get_template_directory_uri() . '/js/libs/bootstrap/js/bootstrap.min.js');
+			wp_register_script('less', get_template_directory_uri() . '/js/libs/less.js');
+			wp_register_script('main', get_template_directory_uri() . '/js/main.js');
+
+			/**
+			 * Laddar skripten vi registrerat ovan.
+			 */
+			wp_enqueue_script('jquery');
+			wp_enqueue_script('modernizr');
+			wp_enqueue_script('bootstrap');
+			wp_enqueue_script('less');
+			wp_enqueue_script('main');
+
+			/**
+			 * load stylesheet
+			 */
+			wp_enqueue_style( 'style', get_stylesheet_uri() );
+			wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/js/libs/bootstrap/css/bootstrap.min.css' );
+			wp_enqueue_style( 'bootstrap-responsive', get_template_directory_uri() . '/js/libs/bootstrap/css/bootstrap-responsive.min.css' );
+		}
+	}
+	add_action('init', 'dazake_load_script');
+?>

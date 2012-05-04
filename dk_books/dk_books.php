b87dd5adec77e050d6b3f20e16c5b36a87383aab
nacheal <nacheal@gmail.com>
2012-05-04 09:14:29 +0000

    add files .

---
+++ dk_books/dk_books.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package DK_BOOKS
+ * @version 1.0
+ */
+/*
+Plugin Name: DK BOOKS
+Plugin URI: http://micheal.dazake.com
+Description: This is a Plugin integration now-reading-redux and mingle plugin showing books .
+Author: Micheal Of Dazake
+Version: 1.0
+Author URI: http://micheal.dazake.com
+*/
+
+
+?>

b86977bef12b8a8fb1209c2690fc8d3eabc053bc
nacheal <nacheal@gmail.com>
2012-05-14 15:21:48 +0000

    可以玩了。

--- branches/2012-5-4-amazon-book-plugin/dk_books/css/main.js
+++ branches/2012-5-4-amazon-book-plugin/dk_books/css/main.js
@@ -25,7 +25,7 @@
       k(this.status);
       return $.ajax({
         type: "POST",
-        url: "test/test.php",
+        url: $('#dk-bookshelf').data('url'),
         data: {
           action: 'statusupdate',
           id: this.id,
@@ -36,18 +36,17 @@
     getContainerWidth('.dk-container-in');
     bookId = 0;
     status = "";
-    return $('.dk-category').sortable({
+    return $('.dk-container-in').sortable({
       items: '.dk-book',
       revert: true,
-      connectWith: '.dk-category',
+      connectWith: '.dk-container-in',
       receive: function(evt, ui) {
-        var $container, $el, $width;
+        var $el, $width;
         $el = $(evt.target);
-        $container = $el.find('.dk-container-in');
-        $width = $container.width();
-        status = $el.data('status');
-        return $container.css({
-          width: $width + 120
+        $width = $el.width();
+        status = $el.parents('.dk-category').data('status');
+        return $el.css({
+          width: $width + 100
         });
       },
       stop: function(evt, ui) {

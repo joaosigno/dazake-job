bc97604988de80c3d00239b717f8851a78482ece
bryantandk <bryantandk@gmail.com>
2012-05-15 06:50:01 +0000

    add old jobs

---
+++ branches/2012-2-7-wfw-blog/yoko/footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Yoko
+ */
+?>
+
+</div><!-- end wrap -->
+
+	<footer id="colophon" class="clearfix">
+		<p>Proudly powered by <a href="http://www.wanfuwang.com">www.万服网.comWordPress</a><span class="sep"></p>
+		<a href="#page" class="top"><?php _e( 'Top', 'yoko' ); ?></a>
+	</footer><!-- end colophon -->
+	
+</div><!-- end page -->
+<?php wp_footer(); ?>
+
+</body>
+</html>

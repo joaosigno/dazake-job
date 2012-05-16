8737aac2bf198a3e61adb7043893b70f473bd485
nacheal@gmail.com <nacheal@gmail.com@6be763a1-2304-9f22-45ae-06bf7fe9b562>
2012-05-16 07:25:53 +0000

    demo

---
+++ branches/2012-5-10-freelancer-blogger-info/dk_blogger_list/dk_blogger_list.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package DK_Blogger_List
+ * @version 1.0
+ */
+/*
+Plugin Name: DK_Blogger_List
+Plugin URI: http://micheal.dazake.com
+Description: This is a Plugin show Blogger.
+Author: Micheal Of Dazake
+Version: 1.0
+Author URI: http://micheal.dazake.com
+*/
+
+// Include other functionality
+require_once dirname(__FILE__) . '/function.php';
+require_once dirname(__FILE__) . '/dk_widget.php';
+
+
+?>
